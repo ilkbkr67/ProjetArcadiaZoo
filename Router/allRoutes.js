1eac77f7dfc557fc11d59e26cb0e4797076a3670
ilkbkr67 <ilkay.bakar@gmail.com>
2024-10-19 23:10:47 +0200

    Mise en place des pages carni, herbi, volant + modif des dossiers

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,6 +4,9 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/habitat", "Nos habitats", "/pages/habitat.html"),
+    new Route("/carnivore", "Les Carnivores", "/pages/carnivore.html"),
+    new Route("/herbivore", "Les Herbivores", "/pages/herbivore.html"),
+    new Route("/volant", "Les Volants", "/pages/volant.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
