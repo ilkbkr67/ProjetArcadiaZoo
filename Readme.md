7c06ac6bb4e36de9e18e37d31da959192952d7ec
ilkbkr67 <ilkay.bakar@gmail.com>
2024-10-20 22:23:58 +0200

    Ajout d'une page pour les herbivores , création de dossier pour mieux organiser les pages, commencement de la page Nos service

--- Readme.md
+++ Readme.md
@@ -1,3 +1,13 @@
 # Zoo Arcadia
 
-Ce site est un site vitrine pour le Zoo Arcadia 
+Ce projet est un ECF proposé par STUDI. Il essaye de répondre au maximum au besoin demandé 
+dans le cahier des charges.
+
+# Réponse au question 
+
+
+
+# Installation
+
+Pour concevoir les différentes page du site j'ai utilisé le logiciel Visual Studio Code. 
+Pour tout se qui concerne la partie front je me suis essentiellement aidé de Bootstrapp. 
--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,12 +2,13 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/habitat", "Nos habitats", "/pages/habitat.html"),
-    new Route("/carnivore", "Les Carnivores", "/pages/carnivore.html"),
-    new Route("/herbivore", "Les Herbivores", "/pages/herbivore.html"),
-    new Route("/volant", "Les Volants", "/pages/volant.html"),
-    new Route("/teddy", "Teddy", "/pages/teddy.html"),
+    new Route("/", "Accueil", "/pages/accueil/home.html"),
+    new Route("/habitat", "Nos habitats", "/pages/accueil/habitat.html"),
+    new Route("/carnivore", "Les Carnivores", "/pages/habitats/carnivore.html"),
+    new Route("/herbivore", "Les Herbivores", "/pages/habitats/herbivore.html"),
+    new Route("/volant", "Les Volants", "/pages/habitats/volant.html"),
+    new Route("/teddy", "Teddy", "/pages/habitats/animaux/teddy.html"),
+    new Route("/cacao", "Cacao", "/pages/habitats/animaux/cacao.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
