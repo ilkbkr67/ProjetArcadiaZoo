9e152ab900e8b224e35ec3273ea163d8878a48b8
ilkbkr67 <ilkay.bakar@gmail.com>
2024-11-14 08:51:22 +0100

    gestion de la page connexion + test Token ok

--- js/auth/signin.js
+++ js/auth/signin.js
@@ -1,88 +1,19 @@
-//Implémenter le JS de ma page
+const mailInput = document.getElementById("EmailInput");
+const passwordInput = document.getElementById("PasswordInput");
+const btnSignin = document.getElementById("btnSignin");
 
-const inputNom = document.getElementById("NomInput");
-const inputPreNom = document.getElementById("PrenomInput");
-const inputMail = document.getElementById("EmailInput");
-const inputPassword = document.getElementById("PasswordInput");
-const inputValidationPassword = document.getElementById("ValidatePasswordInput");
-const btnValidation = document.getElementById("btn-validation-inscription");
+btnSignin.addEventListener("click", checkCredentials);
 
-inputNom.addEventListener("keyup", validateForm); 
-inputPreNom.addEventListener("keyup", validateForm);
-inputMail.addEventListener("keyup", validateForm);
-inputPassword.addEventListener("keyup", validateForm);
-inputValidationPassword.addEventListener("keyup", validateForm);
+function checkCredentials(){
 
-//Function permettant de valider tout le formulaire
-function validateForm(){
-    const nomOk = validateRequired(inputNom);
-    const prenomOk = validateRequired(inputPreNom);
-    const mailOk = validateMail(inputMail);
-    const passwordOk = validatePassword(inputPassword);
-    const passwordConfirmOk = validateConfirmationPassword(inputPassword, inputValidationPassword);
+    if(mailInput.value == "test@mail.com" && passwordInput.value == "123"){
 
-    if(nomOk && prenomOk && mailOk && passwordOk && passwordConfirmOk){
-        btnValidation.disable = false;
+            const token = "mdvjdsmcjsqomsqkosqkxosqk";
+            setToken(token);
+            window.location.replace("/");
     }
     else{
-        btnValidation.disable = true;
-    }
-}
-
-function validatePassword(input){
-    //Définir mon regex
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])[A-Za-z\d\W_]{8,}$/;
-    const passwordUser = input.value;
-    if(passwordUser.match(passwordRegex)){
-        input.classList.add("is-valid");
-        input.classList.remove("is-invalid"); 
-        return true;
-    }
-    else{
-        input.classList.remove("is-valid");
-        input.classList.add("is-invalid");
-        return false;
-    }
-}
-
-function validateConfirmationPassword(inputPwd, inputConfirmPwd){
-    if(inputPwd.value == inputConfirmPwd.value){
-        inputConfirmPwd.classList.add("is-valid");
-        inputConfirmPwd.classList.remove("is-invalid");
-        return true;
-    }
-    else{
-        inputConfirmPwd.classList.add("is-invalid");
-        inputConfirmPwd.classList.remove("is-valid");
-        return false;
-    }
-}
-
-function validateMail(input){
-    //Définir mon regex
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    const mailUser = input.value;
-    if(mailUser.match(emailRegex)){
-        input.classList.add("is-valid");
-        input.classList.remove("is-invalid"); 
-        return true;
-    }
-    else{
-        input.classList.remove("is-valid");
-        input.classList.add("is-invalid");
-        return false;
-    }
-}
-
-function validateRequired(input){
-    if(input.value != ''){
-        input.classList.add("is-valid");
-        input.classList.remove("is-invalid"); 
-        return true;
-    }
-    else{
-        input.classList.remove("is-valid");
-        input.classList.add("is-invalid");
-        return false;
+        mailInput.classList.add("is-invalid");
+        passwordInput.classList.add("is-invalid");
     }
 }
